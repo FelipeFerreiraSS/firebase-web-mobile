e4feed2f2597945e352de81a5df91e79419d0aef
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2023-07-28 00:18:39 -0300

    Update _app.js

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,5 +1,14 @@
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 import '@/styles/globals.css'
+import { AuthProvider } from '../context/AuthContext'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return ( 
+    <AuthProvider>
+      <Header />
+        <Component {...pageProps} />
+      <Footer />
+    </AuthProvider>
+  )
 }
